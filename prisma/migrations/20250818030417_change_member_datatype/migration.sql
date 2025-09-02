6ed2fe74af21d72a1d60bc26c13254ab8a07f480
chuleeshen <leeshen.chu@monash.edu>
2025-09-02 16:04:00 +0800

    refactor: remove project members table migrations

--- prisma/migrations/20250818030417_change_member_datatype/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `members` on the `Project` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Project" DROP COLUMN "members",
-ADD COLUMN     "members" JSON NOT NULL;
