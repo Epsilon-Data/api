23dace2ce1103117f599d6b6b2285248ba370cfa
Aare Puussaar <aare.puussaar@gmail.com>
2025-11-11 17:19:09 +0000

    chore: cleanup of pointless tests
    These need to be replaced with proper tests

--- src/common/filters/auth.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthExceptionFilter } from './auth.filter';
-
-describe('AuthFilter', () => {
-  it('should be defined', () => {
-    expect(new AuthExceptionFilter()).toBeDefined();
-  });
-});
