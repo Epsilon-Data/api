e6595d42ba4aa8ef61de0e304567b4efbd40ed61
Aare Puussaar <aare.puussaar@gmail.com>
2026-01-09 15:42:41 +0000

    fix: added missing module

--- src/coordinator/coordinator.module.ts
+++ src/coordinator/coordinator.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoordinatorController } from './coordinator.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AnalysisRequestModule } from 'src/analysis-request/analysis-request.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AnalysisRequestModule],
   controllers: [CoordinatorController],
 })
 export class CoordinatorModule {}
