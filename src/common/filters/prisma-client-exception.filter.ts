7e3f0166a3bef604f46b3a8fcfee7f36e625b7d0
chuleeshen <leeshen.chu@monash.edu>
2026-09-08 12:57:34 +0800

    refactor: update prisma exception filter to use @prisma/client

--- src/common/filters/prisma-client-exception.filter.ts
+++ src/common/filters/prisma-client-exception.filter.ts
@@ -10,7 +10,7 @@ import {
   BadRequestException,
   InternalServerErrorException,
 } from '@nestjs/common';
-import { Prisma } from 'src/generated/prisma/client';
+import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 
 interface GenericErrorResponseBody {
